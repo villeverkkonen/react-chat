6fa3f0bc0672d4a79c8ba5a99d03ecdd88c18dc4
Ville Verkkonen <ville.verkkonen@solidabis.com>
2020-03-24 23:19:59 +0200

    Frontend build back to repo

---
+++ frontend/build/precache-manifest.8486d0c2e08a61d83d1ba23d5ddabf6a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "917a8411bca92729d5bef7f6e3cc583d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58fe1329b06c4ade326c",
+    "url": "/static/js/2.1608fabd.chunk.js"
+  },
+  {
+    "revision": "81d54293b30b81e01bb47f8cce3e382c",
+    "url": "/static/js/2.1608fabd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ac1dc7b21c8617473df",
+    "url": "/static/js/main.2e0588e0.chunk.js"
+  },
+  {
+    "revision": "b4bb80aa4ebdace41764",
+    "url": "/static/js/runtime-main.f069f5f1.js"
+  }
+]);
